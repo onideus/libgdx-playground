1d27d07eda8f3221f1d2a406d35a0b009f3ebcfd
Zach Martin <zach@zach-martin.com>
2019-11-07 22:24:13 -0500

    Added first 'game' (term used loosely).

--- desktop/src/com/onideus/game/desktop/DesktopLauncher.java
+++ desktop/src/com/onideus/game/desktop/DesktopLauncher.java
@@ -2,11 +2,12 @@ package com.onideus.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.onideus.game.HelloWorldImage;
 import com.onideus.game.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+		new LwjglApplication(new HelloWorldImage(), config);
 	}
 }
